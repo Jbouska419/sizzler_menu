a442fecfc2ca74ae38c0a80d87a12690960f13a4
James Bouska <jbouska419@gmail.com>
2014-08-20 15:44:08 -0500

    added files to project

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sizzle Chest</title>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta name="generator" content="Geany 0.20" />
+<link rel="stylesheet" type="text/css" href="css/load.css">
+<script src="bower_components/sizzle/dist/sizzle.min.js" type="text/javascript"></script>
+<script src="bower_components/respond/dest/respond.min.js" type="text/javascript"></script>
+<script src="bower_components/html5shiv/dist/html5shiv.min.js" type="text/javascript"></script>
+
+
+</head>
+
+<body>
+	<nav class="navigation">
+		<div class="wrapper">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div id="menu-collapse">
+				<a class="show" href="#menu-collapse">&#9776;</a>
+				<a class="hide" href="#">&#9776;</a>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">FAQ</a></li>
+				<li><a href="#">Photos</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			</div>
+		</div>
+    
+	</nav>
+	
+</body>
+
+</html>
